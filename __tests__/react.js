c01ef44a2faefa8d1aacf6cb41b5eaa4c1822418
Andy Lam <axm.lam@gmail.com>
2023-11-08 14:31:48 -0800

    finished button test

--- __tests__/react.js
+++ __tests__/react.js
@@ -22,13 +22,18 @@ describe('Unit testing React components', () => {
     test('Renders three average medical expense forms', async () => {
       const forms = await screen.findAllByRole('textbox');
       expect(forms.length).toBe(3);
+    });
+
+    test('medical expense forms have correct label', async () => {
+      const forms = await screen.findAllByRole('textbox');
       expect(forms[0].previousSibling).toHaveTextContent('What were your average medical expenses in 2023?')
       expect(forms[0].nextSibling).toHaveTextContent('What were your average medical expenses in 2022?')
       expect(forms[1].nextSibling).toHaveTextContent('What were your average medical expenses in 2021?')
-    });
+    })
 
-    test('Contains a submit button', () => {
-
+    test('Contains a submit button', async () => {
+      const button = await screen.findAllByRole('button')
+      expect(button.length).toBe(1);
     });
 
     test('Functions are invoked on click', async () => {
