200cbdbeccb85edd1619ef28b4aa242402263ae6
Andy Lam <axm.lam@gmail.com>
2023-11-08 14:16:45 -0800

    got jest and react library functions working

--- server/controllers/model.js
+++ server/controllers/model.js
@@ -2,6 +2,12 @@ const { Client } = require('pg'); // Use 'Client' instead of 'DB'
 
 const PG_URI = 'postgres://unbrdbbu:YlyeXVScMxCm2eFHy-ANcjRLiWdfUK4C@bubble.db.elephantsql.com/unbrdbbu';
 
+// host name/address: bubble.db.elephantsql.com
+// maintenance and name: unbrdbbu
+// password: YlyeXVScMxCm2eFHy-ANcjRLiWdfUK4C
+
+
+
 const client = new Client({
   connectionString: PG_URI
 });
