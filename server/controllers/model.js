4b7f2d3360aa7ce083a6d29430433ffa69bbd816
Sung Ku Kang <sungkuka@gmail.com>
2023-11-08 14:23:41 -0800

    changed db uri

--- server/controllers/model.js
+++ server/controllers/model.js
@@ -1,16 +1,21 @@
 const { Client } = require('pg'); // Use 'Client' instead of 'DB'
 
-const PG_URI = 'postgres://unbrdbbu:YlyeXVScMxCm2eFHy-ANcjRLiWdfUK4C@bubble.db.elephantsql.com/unbrdbbu';
+// previous group's URI
+// const PG_URI = 'postgres://unbrdbbu:YlyeXVScMxCm2eFHy-ANcjRLiWdfUK4C@bubble.db.elephantsql.com/unbrdbbu';
+
+// our URI
+const PG_URI =
+  'postgres://rbnzayte:w5w_t5q23ejXEShRkeJALXfAUbgCDIad@bubble.db.elephantsql.com/rbnzayte';
 
 const client = new Client({
-  connectionString: PG_URI
+  connectionString: PG_URI,
 });
 
-client.connect(); 
+client.connect();
 
 module.exports = {
   query: (text, params, callback) => {
     console.log('executed query', text);
     return client.query(text, params, callback);
-  }
+  },
 };
